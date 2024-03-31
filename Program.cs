d65229267c6b774c4fdcfdba21129bd36693b386
shuabruh <shuabruh@student.telkomuniversity.ac.id>
2024-03-31 20:55:43 +0700

    Tambahkan class DataMahasiswa1302220072

--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
-﻿internal class Program
+﻿using System;
+using System.Text.Json;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        readJSON();
+    }
+    public static void readJSON()
+    {
+        string filepath = Path.Combine("tp7_1_1302220072.json");
+        string jsonFile = File.ReadAllText(filepath);
+        DataMahasiswa1302220072.DataMhs mhsdata = JsonSerializer.Deserialize<DataMahasiswa1302220072.DataMhs>(jsonFile);
+
+        Console.WriteLine("Nama " + mhsdata.nama.depan + " " + mhsdata.nama.belakang);
+        Console.WriteLine("nim " + mhsdata.nim);
+        Console.WriteLine("fakultas " + mhsdata.fakultas);
+    }
+}
+
+public class DataMahasiswa1302220072
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class DataMhs
+    {
+        public Nama nama { get; set; }
+        public int nim { get; set; }
+        public string fakultas { get; set; }
     }
 }

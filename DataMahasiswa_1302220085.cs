f9941b26b3999c27eb9a8a6f98088c27e2e00636
myousefg <myousefg@gmail.com>
2024-03-31 22:01:43 +0700

    tambahkan class

---
+++ DataMahasiswa_1302220085.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DataMahasiswa
+{
+    public class DataMahasiswa1302220085
+    {
+        public class nama
+        {
+            public string depan { get; set; }
+            public string belakang { get; set; }
+        }
+
+        public class data
+        {
+            public nama nama { get; set; }
+            public int nim { get; set; }
+            public string fakultas { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using DataMahasiswa;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        readJSON();
+    }
+
+    public static void readJSON()
+    {
+        string filepath = Path.Combine("tp7_1_1302220085.json")
+        string jsonFile = File.ReadAllText(filepath);
+        DataMahasiswa1302220085.data data = JsonSerializer.Deserialize<DataMahasiswa1302220085.data>(jsonFile);
+
+        Console.WriteLine("Nama " + data.nama.depan + " " + data.nama.belakang);
+        Console.WriteLine("nim " + data.nim);
+        Console.WriteLine("fakultas " + data.fakultas);
     }
 }

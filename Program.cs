d991833349aaa610b8b774b42acdd27186188a7d
aufa <abdillahaufa79@gmail.com>
2024-03-31 23:39:59 +0700

    menambahkan class DataMahasiswa

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using System.Text.Json;
+using DataMahasiswa;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        readJSON();
+    }
+
+    public static void readJSON()
+    {
+        string filepath = Path.Combine(@"C:\Users\delll\Documents\Kuliah S1-4\KPL-VLY\Praktikum KPL\tpmodul7_1302220131\tpmodul7_1302220131\tpmodul7_kelompok_2\tp7_1_1302220131.json");
+        string jsonFile = File.ReadAllText(filepath);
+        DataMahasiswa1302220131_Aufa.data data = JsonSerializer.Deserialize<DataMahasiswa1302220131_Aufa.data>(jsonFile);
+
+        Console.WriteLine("Nama :" + data.nama.depan + " " + data.nama.belakang);
+        Console.WriteLine("Nim :" + data.nim);
+        Console.WriteLine("Fakultas :" + data.fakultas);
     }
 }
